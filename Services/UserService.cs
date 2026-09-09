81f095a17f4723ff77a95b615d1aebf108069080
DeveloperNatan <natandesouza403@gmail.com>
2026-09-09 14:32:18 -0300

    fix: names DTOs

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -7,7 +7,6 @@ using Microsoft.IdentityModel.Tokens;
 using TimeRecord.Data;
 using TimeRecord.DTO.Users;
 using TimeRecord.DTO.Login;
-using TimeRecord.DTO.Users;
 using TimeRecord.Exceptions;
 using TimeRecord.Models;
 
